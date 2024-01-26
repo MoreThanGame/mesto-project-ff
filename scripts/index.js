1319aeceea5d4d767294f52cf5549b64c697463c
MoreThanGame <antonpleshakov.job@gmail.com>
2024-01-26 14:57:37 +0300

    2 отправка

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,39 @@
 
 // @todo: Функция создания карточки
 
-
 const content = document.querySelector(".content");
 const placesList = content.querySelector(".places__list");
 const addButton = content.querySelector(".profile__add-button");
 
-const deleteCard = (element) => {
-  element.target.parentElement.remove();
+const deleteCard = (evt) => {
+  evt.target.closest(".card").remove();
 };
 
-function addCard(nameValue, linkValue, deleteCard) {
+function addCard(cardData) {
+  const { nameValue, linkValue, deleteCard } = cardData;
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
   cardElement.querySelector(".card__title").textContent = nameValue;
   cardElement.querySelector(".card__image").src = linkValue;
+  cardElement.querySelector(".card__image").alt = `Фото ${nameValue}`;
 
-  placesList.append(cardElement);
   deleteButton.addEventListener("click", deleteCard);
+
+  return cardElement;
 }
 
 initialCards.forEach((element) => {
-  addCard(element.name, element.link, deleteCard);
-});
+  const newCard = addCard({
+    nameValue: element.name,
+    linkValue: element.link,
+    deleteCard: deleteCard,
+  });
 
+  placesList.append(newCard);
+  
+});
 
 // @todo: Функция удаления карточки
 
