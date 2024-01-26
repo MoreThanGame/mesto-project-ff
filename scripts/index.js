61b22bb1f1f15cf61a97ef609dce909248e095bc
MoreThanGame <antonpleshakov.job@gmail.com>
2024-01-26 11:56:33 +0300

    1 отправка исправленная

--- scripts/index.js
+++ scripts/index.js
@@ -4,31 +4,32 @@
 
 // @todo: Функция создания карточки
 
-const content = document.querySelector('.content');
-const placesList = content.querySelector('.places__list');
-const addButton = content.querySelector('.profile__add-button');
+
+const content = document.querySelector(".content");
+const placesList = content.querySelector(".places__list");
+const addButton = content.querySelector(".profile__add-button");
 
 const deleteCard = (element) => {
-    element.target.parentElement.remove();
-}
+  element.target.parentElement.remove();
+};
 
 function addCard(nameValue, linkValue, deleteCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    
-    cardElement.querySelector('.card__title').textContent = nameValue;
-    cardElement.querySelector('.card__image').src = linkValue;
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-    placesList.append(cardElement);
-    deleteButton.addEventListener('click', deleteCard);
+  cardElement.querySelector(".card__title").textContent = nameValue;
+  cardElement.querySelector(".card__image").src = linkValue;
+
+  placesList.append(cardElement);
+  deleteButton.addEventListener("click", deleteCard);
 }
 
-initialCards.forEach(element => {
-    addCard(element.name, element.link, deleteCard);
+initialCards.forEach((element) => {
+  addCard(element.name, element.link, deleteCard);
 });
+
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-
